6ae32a7b313763c96783b221ddf634ea4f2cb153
Gleb Korzan <gleb.korzan@gmail.com>
2022-06-14 14:55:49 +0300

    fix: catching not existing filename

--- utils/readCityList.js
+++ utils/readCityList.js
@@ -1,10 +1,12 @@
 import { readFile } from "fs/promises";
 
 const readCityList = async (path) => {
-  const json = JSON.parse(
-    await readFile(new URL(path, import.meta.url))
-  );
-  return json;
+  try {
+    const json = JSON.parse(await readFile(new URL(path, import.meta.url)));
+    return json;
+  } catch (_) {
+    return [];
+  }
 };
 
 export { readCityList };
